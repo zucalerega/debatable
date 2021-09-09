dd3ac45395a2acec5b8edeee8ae2831bb5f7451e
zucalerega <85199881+zucalerega@users.noreply.github.com>
2021-09-08 19:22:21 -0500

    Update settings.py

--- djangodebatable/debatable/settings.py
+++ djangodebatable/debatable/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b&2kuksn62r+=%ie8iadnpmr04ddanf0vls*c1sh@*6=l+xza&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
