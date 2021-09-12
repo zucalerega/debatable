7c4b003dcf5a00942a847a30f71fe15f0e4c8f00
zucalerega <85199881+zucalerega@users.noreply.github.com>
2021-09-12 16:37:45 -0500

    Delete djangodebatable/pages directory

--- djangodebatable/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import ResourceListView, ResourceDetailView, resource_list_view
-
-app_name = 'resources'
-urlpatterns = [
-    path('', resource_list_view, name='resource-list'),
-    path('<int:pk>/', ResourceDetailView.as_view(), name="resource")
-]
