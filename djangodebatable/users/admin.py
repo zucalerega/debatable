dd652f4c3722205f28ecd76579d8ee737e7ce971
zucalerega <85199881+zucalerega@users.noreply.github.com>
2021-10-27 22:32:58 -0500

    Update admin.py

--- djangodebatable/users/admin.py
+++ djangodebatable/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Report, Follow
+from .models import Profile, Report, Follow, Feedback
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Report)
 admin.site.register(Follow)
+admin.site.register(Feedback)
